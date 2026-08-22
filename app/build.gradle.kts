d76d2a844f53839797493cd1cd6ab06aeb131b8e
Neura Tech AI <samarthpathak62@gmail.com>
2026-08-22 11:00:47 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.jarvis.rgai"
+    applicationId = "com.ai.jarvis"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
